397d37abbae01eb6a5b7ad5f3634aed7a64e6ebd
Luis Lema <info@lemaluis.com>
2023-03-12 18:01:57 -0500

    Initial emition of QuartzConsoleApp project to test Quartz.NET library.

---
+++ QuartzConsoleApp/HelloJob.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Quartz;
+
+namespace QuartzConsoleApp
+{
+    internal class HelloJob : IJob
+    {
+        public async Task Execute(IJobExecutionContext context)
+        {
+            await Console.Out.WriteLineAsync("This job is used to make greetings!");
+        }
+    }
+}
---
+++ QuartzConsoleApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using Quartz;
+using Quartz.Impl;
+using QuartzConsoleApp;
+
+StdSchedulerFactory factory = new StdSchedulerFactory();
+IScheduler scheduler=await factory.GetScheduler();
+
+await scheduler.Start();
+
+// Define a job of type: HelloJob
+IJobDetail jobDetail =JobBuilder.Create<HelloJob>()
+    .WithIdentity("job1", "group1")
+    .Build();
+
+// This trigger makes job to run now and repeat every 5 seconds
+// You can configure multiple triggers for a the same job
+ITrigger trigger=TriggerBuilder.Create()
+    .WithIdentity("trigger1","group1")
+    .StartNow()
+    .WithSimpleSchedule(x=>x
+        .WithIntervalInSeconds(5)
+        .RepeatForever())
+    .Build();
+
+// Schedule job for execution
+await scheduler.ScheduleJob(jobDetail, trigger);
+
+await Task.Delay(10000);
+
+// Any running job will be killed after scheduler shutdown
+await scheduler.Shutdown();
+
