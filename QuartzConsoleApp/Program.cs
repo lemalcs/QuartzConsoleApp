76c41ec712c196946d1985198e3562b2682f9940
Luis Lema <info@lemaluis.com>
2023-03-14 23:37:11 -0500

    Set SQL Server as the backing storage for Quartz.NET jobs.

--- QuartzConsoleApp/Program.cs
+++ QuartzConsoleApp/Program.cs
@@ -5,17 +5,26 @@ using QuartzConsoleApp;
 StdSchedulerFactory factory = new StdSchedulerFactory();
 IScheduler scheduler=await factory.GetScheduler();
 
+
 await scheduler.Start();
 
 // Define a job of type: HelloJob
 IJobDetail jobDetail =JobBuilder.Create<HelloJob>()
-    .WithIdentity("job1", "group1")
+    // with persistent storage, the next line creates the job the first time it's
+    // executed, otherwise a Quartz.JobPersistenceException exception is raised
+    // because the job already exists, as a workaround you can delete the job
+    // before creating a new one
+    //.WithIdentity("job1", "group1")
+    .UsingJobData("job1","group1") // this will create a new job when using persistent storage
     .Build();
 
 // This trigger makes job to run now and repeat every 5 seconds
 // You can configure multiple triggers for a the same job
 ITrigger trigger=TriggerBuilder.Create()
-    .WithIdentity("trigger1","group1")
+    // This creates a trigger, if using persistent storage is used, a Quartz.ObjectAlreadyExistsException
+    // exception is raised if trigger already exists
+    //.WithIdentity("trigger1","group1") 
+    .UsingJobData("trigger1","group1") // this creates a new trigger in persistent storage
     .StartNow()
     .WithSimpleSchedule(x=>x
         .WithIntervalInSeconds(5)
